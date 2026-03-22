359e1d847c320bb22c04c1375819d03b63faa377
rajeshkanna-36 <your.email@gmail.com>
2026-03-22 16:11:25 +0530

    Initial Commit

---
+++ src/test/java/com/example/Resume/Parser/ResumeParserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Resume.Parser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumeParserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
